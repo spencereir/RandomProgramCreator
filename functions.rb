4fbcfe0740184cc1ab9a022769728a89f682da41
Spencer Whitehead <spencer.whitehead@rogers.com>
2014-07-12 18:25:13 -0400

    Update functions.rb

--- functions.rb
+++ functions.rb
@@ -1,20 +1,20 @@
 class Function
   @@num_functions = 0
+  @function = ""
   @name = ""
   @return_val = ""
   @prototype = ""
   @num_params = 0
   @vars = Array.new()
   @var_types = Array.new()
-  def initialize (name, return_val, num_params, vars, var_types) 
-    @name = name
+  def initialize (function, name, return_val, num_params, vars, var_types) 
+    @function = function
+    @old_name = name
     @return_val = return_val
     @num_params = num_params
     @vars = vars
     @var_types = var_types
-    if (@name.eql? "main")    # Main is a dumb name for a function. Lets cool it up a bit
-      @name = (0...6).map { (65 + rand(26)).chr }.join
-    end
+    @name = @old_name.to_s + (0..2).map { (65 + rand(26)).chr }.join
     @@num_functions += 1
     @prototype = @return_val + " " + @name + "("
     @num_params.times do |i|
@@ -24,9 +24,13 @@ class Function
       end
     end
     @prototype += ");"
+    @function.gsub!(@old_name, @name)
   end  
   def num
     @@num_functions
+  end
+  def function
+    @function
   end
   def rType
     @return_val
@@ -34,16 +38,46 @@ class Function
   def name
     @name
   end
+  def oldName
+    @old_name
+  end
   def numParams
     @num_params
   end
-  def vars(i)
+  def vars
+    @vars
+  end
+  def sVars(i)
     @vars[i]
   end
-  def varTypes(i)
-    @varTypes[i]
+  def varTypes
+    @var_types
+  end
+  def sVarTypes(i)
+    @var_types[i]
   end
   def prototype
     @prototype
+  end
+  def call(params, mode)
+    if (mode == 1)
+      @call = @name + "("
+    elsif (mode == 0)
+      @call = @old_name + "("
+    end
+    if(params.length > 0)
+      params.length.times do |i|
+        @call += params[i]
+        if (i < params.length - 1) 
+          @call += ", "
+        end
+      end
+    end
+    @call += ");\n"
+    @call
+  end
+  def replaceName(f)
+    empty = []
+    @function.gsub!(f.call(empty, 0), f.call(empty, 1))
   end
 end
